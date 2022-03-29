d800c0940f57d5129d69ed07e8ca6262088b67e8
camonunez18 <canunezz35@gmail.com>
2022-03-28 20:29:07 -0500

    Add files via upload

---
+++ logica/tiple.py
@@ -0,0 +1,9 @@
+from logica.instrumento import Instrumento
+
+
+class Tiple(Instrumento):
+    def tocar(self):
+        print("Tocando tiple")
+
+    def afinar(self):
+        print("Afinando tiple")
